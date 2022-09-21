35bbf99f96fe1a2bc911844bd2e6190159cb9752
EderSampayo <101659210+EderSampayo@users.noreply.github.com>
2022-09-21 09:59:25 +0200

    Add files via upload

---
+++ src/main/java/com/zetcode/Usuario.java
@@ -0,0 +1,13 @@
+package com.zetcode;
+
+public class Usuario
+{
+    private String nombre;
+    private String clave;
+
+    public Usuario(String pNombre, String pClave)
+    {
+        this.nombre = pNombre;
+        this.clave = pClave;
+    }
+}
